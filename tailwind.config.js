7122451039178889870d77c876bcd6a0fd5bd839
fbgouveia <fbgouveia@gmail.com>
2025-11-25 22:16:55 +1000

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        primary: '#0071e3',
+        surface: '#f5f5f7',
+      }
+    },
+  },
+  plugins: [],
+}
