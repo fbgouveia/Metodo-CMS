91a06369361b8b20022377b628f13b430dd7db30
fbgouveia <fbgouveia@gmail.com>
2025-11-25 23:34:05 +1000

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // Importante para o Tailwind funcionar
+import './index.css'; // Importante para o estilo
 
-const rootElement = document.getElementById('root');
-
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  console.error("Elemento 'root' não encontrado no HTML");
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
