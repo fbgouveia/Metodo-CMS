fa5038bbea8d3ec10dffc533ebe31ad530513eeb
fbgouveia <fbgouveia@gmail.com>
2025-11-25 21:40:18 +1000

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 
+// Vamos tentar renderizar algo sem importar NENHUM componente complexo
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (!rootElement) {
+  document.body.innerHTML = '<h1>ERRO CRÍTICO: A div com id "root" não existe no index.html</h1>';
+} else {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <div style={{ padding: 50, fontFamily: 'sans-serif', textAlign: 'center' }}>
+      <h1 style={{ color: 'green' }}>SUCESSO! O React está vivo.</h1>
+      <p>O problema não é a configuração, é algum componente interno.</p>
+    </div>
+  );
+}
