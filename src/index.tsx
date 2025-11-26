17b983def0092926c9fb1a7383c1064c7f8a0847
fbgouveia <fbgouveia@gmail.com>
2025-11-26 11:44:11 +1000

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css' // <--- ESSENCIAL
 
-console.log("Tentando iniciar o React..."); // Mensagem no console
-
-const rootElement = document.getElementById('root');
-
-if (rootElement) {
-  console.log("Elemento ROOT encontrado!");
-  const root = ReactDOM.createRoot(rootElement);
-  
-  // Tenta renderizar o App com proteção de erro
-  try {
-    root.render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    );
-    console.log("React renderizado com sucesso");
-  } catch (error) {
-    console.error("ERRO AO RENDERIZAR APP:", error);
-    rootElement.innerHTML = '<h1 style="color:red; font-size:30px">ERRO FATAL NO APP. CHEQUE O CONSOLE.</h1>';
-  }
-} else {
-  console.error("FATAL: Não encontrei a div 'root' no HTML.");
-  document.body.innerHTML = '<h1 style="color:red; font-size:30px">ERRO FATAL: Faltou a div ROOT no HTML.</h1>';
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
